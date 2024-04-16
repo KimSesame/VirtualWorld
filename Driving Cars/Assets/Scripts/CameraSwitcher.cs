6cc4881380449c45887cc14d3c75817710f34df9
KimSesame <kimbs082787@gamil.com>
2024-04-16 20:23:15 +0900

    Move camera properly when player' car rotating and Switch view left shift to previous view, right shift to next one.

--- Driving Cars/Assets/Scripts/CameraSwitcher.cs
+++ Driving Cars/Assets/Scripts/CameraSwitcher.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 enum ViewType : int
 {
     FollowPlayer = 0,
-    FisrtPerson = 1,
+    FirstPerson = 1,
     Top = 2
 }
 
@@ -15,20 +15,17 @@ public class CameraSwitcher : MonoBehaviour
     public GameObject player;
 
     private Vector3[] offset = { new(0, 5, -10), new(0, 2, 1), new(0, 30, 10) };
-    private ViewType viewType = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    private ViewType viewType = ViewType.FollowPlayer;  // set follow player as default view
 
     // Update for check camera type
     void Update()
     {
-        // If Left or Right Shift down, change view type
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        // If Right Shift down, change view to next
+        if (Input.GetKeyDown(KeyCode.RightShift))
             viewType = (ViewType)(((int)viewType + 1) % Enum.GetNames(typeof(ViewType)).Length);
+        // If Left Shift down, change view to previous
+        else if (Input.GetKeyDown(KeyCode.LeftShift))
+            viewType = (ViewType)(((int)viewType + Enum.GetNames(typeof(ViewType)).Length - 1) % Enum.GetNames(typeof(ViewType)).Length);
     }
 
     // LateUpdate is called after Update method, which allows to more smoothly follow the player
@@ -36,24 +33,26 @@ public class CameraSwitcher : MonoBehaviour
     {
         switch (viewType)
         {
+            // following player view
             case ViewType.FollowPlayer:
-                transform.position = player.transform.position + offset[(int)ViewType.FollowPlayer];
+                transform.position = player.transform.TransformPoint(offset[(int)ViewType.FollowPlayer]);
                 transform.rotation = player.transform.rotation * Quaternion.Euler(5, 0, 0);
                 return;
 
-            case ViewType.FisrtPerson:
-                transform.position = player.transform.position + offset[(int)ViewType.FisrtPerson];
+            // FPS view
+            case ViewType.FirstPerson:
+                transform.position = player.transform.TransformPoint(offset[(int)ViewType.FirstPerson]);
                 transform.rotation = player.transform.rotation * Quaternion.Euler(5, 0, 0);
                 return;
 
+            // TOP view
             case ViewType.Top:
-                transform.position = player.transform.position + offset[(int)ViewType.Top];
+                transform.position = player.transform.TransformPoint(offset[(int)ViewType.Top]);
                 transform.rotation = player.transform.rotation * Quaternion.Euler(90, 0, 0);
                 return;
 
             default:
                 return;
         }
-
     }
 }
